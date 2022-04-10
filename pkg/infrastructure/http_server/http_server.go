f14aed6a81f25f15c701da7e8046bb81bfdeccdd
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-09 21:02:44 -0300

    refactor: http_server

--- pkg/infrastructure/http_server/http_server.go
+++ pkg/infrastructure/http_server/http_server.go
@@ -1,17 +1,39 @@
 package httpserver
 
 import (
+	"base/pkg/application/interfaces"
 	"errors"
+	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+type IHTTPServer interface {
+	Setup()
+	RegisterRoute(method string, endpoint string, handler ...gin.HandlerFunc) error
+	Run() error
+}
+
 type HTTPServer struct {
-	router *gin.Engine
+	router  *gin.Engine
+	logger  interfaces.ILogger
+	address string
+	server  *http.Server
 }
 
 func (server *HTTPServer) Setup() {
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	server.address = fmt.Sprintf("%s:%s", host, port)
+
 	server.router = gin.New()
+
+	server.server = &http.Server{
+		Addr:    server.address,
+		Handler: server.router,
+	}
 }
 
 func (server *HTTPServer) RegisterRoute(method string, endpoint string, handler ...gin.HandlerFunc) error {
@@ -30,5 +52,19 @@ func (server *HTTPServer) RegisterRoute(method string, endpoint string, handler 
 		return errors.New("Method now allowed")
 	}
 
+	return nil
+}
+
+func (server *HTTPServer) Run() error {
+	certPath := os.Getenv("TLS_CERT_PATH")
+	keyPath := os.Getenv("TLS_KEY_PATH")
+
+	err := server.server.ListenAndServeTLS(certPath, keyPath)
+	if err != nil {
+		server.logger.Error(fmt.Sprintf("Error while trying to serve HTTP: %s", err.Error()))
+		return err
+	}
+
+	server.logger.Info(fmt.Sprintf("Server running at https://%s", server.address))
 	return nil
 }
